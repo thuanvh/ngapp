a3caf3f9efc43278de51fc16f91b4d0a338a7105
Thuan <thuanvh@gmail.com>
2020-09-24 15:08:07 +0700

    Update build button with icon

--- angular-j2k/build_android.vi.bat
+++ angular-j2k/build_android.vi.bat
@@ -1,4 +1,6 @@
-ng build --prod --localize --base-href . --output-path ../local_temp/
+:: ng build --prod --localize --base-href . --output-path ../local_temp/
+
+ng build --configuration=production,vi --base-href / --output-path ../local_temp/
 
 del ..\j2kroid\www\*.* /F /Q /S
 xcopy ..\local_temp\vi ..\j2kroid\www /h /i /c /k /e /r /y
---
+++ angular-j2k/copy_android.vi.bat
@@ -0,0 +1,2 @@
+del ..\j2kroid\www\*.* /F /Q /S
+xcopy ..\local_temp\vi ..\j2kroid\www /h /i /c /k /e /r /y
